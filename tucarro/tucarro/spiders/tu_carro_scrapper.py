8c94df2dc5baddb0fc1fde42b8ce3c1f7bca580e
Julian Uran <julianx@gmail.com>
2018-05-19 14:13:25 -0400

    Removing maker and model parsing since those are already being parsed using the cars info data.

--- tucarro/tucarro/spiders/tu_carro_scrapper.py
+++ tucarro/tucarro/spiders/tu_carro_scrapper.py
@@ -10,25 +10,13 @@ class BlogSpider(scrapy.Spider):
         # for maker in response.css('dl#id_9991744-AMCO_1744_1.filters__brand dd'):  # Popular only
         for maker in response.css('.modal-location-filter-9991744-AMCO_1744_1 .location_filter_inner'):  # All makers
             maker_url = response.urljoin(maker.css('a::attr("href")').extract_first(default=''))
-            maker_name = maker.css("a::text").extract_first(default='').strip()
-            req = scrapy.Request(url=maker_url, callback=self.parse_maker, meta={"maker": maker_name})
-            yield req
-
-    def parse_maker(self, response):
-        # for model in response.css('dl#id_9991744-AMCO_1744_2.filters__model dd'):  # Popular only
-        for model in response.css('.modal-location-filter-9991744-AMCO_1744_2 .location_filter_inner'):  # All models
-            meta = response.meta
-            meta["model"] = model.css("a::text").extract_first(default='').strip()
-            req = scrapy.Request(url=response.urljoin(model.css('a::attr("href")').extract_first(default='')),
-                                 callback=self.parse_grid,
-                                 meta=meta)
+            req = scrapy.Request(url=maker_url, callback=self.parse_grid)
             yield req
 
     def parse_grid(self, response):
         for item in response.css('.results-item'):
             req = scrapy.Request(url=response.urljoin(item.css('a::attr("href")').extract_first(default='')),
-                                 callback=self.parse_car,
-                                 meta=response.meta)
+                                 callback=self.parse_car)
             yield req
 
     def parse_car(self, response):
@@ -72,9 +60,6 @@ class BlogSpider(scrapy.Spider):
             'year': response.css('.vip-classified-info dd::text').extract_first(default=''),
             'kms': kms,
             'link': response.request.url,
-            # 'img': response.css('.gallery-trigger img::attr(src)').extract_first(default=''),
-            'maker': response.meta.get('maker'),
-            'model': response.meta.get('model'),
             'location': location,
             'description': description,
             'phone_number': phone_number
